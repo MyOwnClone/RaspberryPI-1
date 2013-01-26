95b2ed8d5a179a4b692691d6d8db7297e1dfca85
kwigbo <jed@kwigbo.com>
2013-01-25 22:50:51 -0500

    Removed build files

--- GLCube/Classes/Cube.m
+++ GLCube/Classes/Cube.m
@@ -1,3 +1,30 @@
+/*
+Copyright (c) 2012, Broadcom Europe Ltd
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holder nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #import "Cube.h"
 
 #import <stdio.h>
@@ -274,16 +301,6 @@ static GLfloat inc_and_clip_distance(GLfloat distance, GLfloat distance_inc);
     state->tex_buf1 = malloc(image_sz);
     state->tex_buf2 = malloc(image_sz);
     state->tex_buf3 = malloc(image_sz);
-
-    NSFileManager *fm = [NSFileManager defaultManager];
-    
-    BOOL isDir = NO;
-    if ([fm fileExistsAtPath:PATH_STRING isDirectory:&isDir] && isDir)
-    { 
-       NSLog(@"SubPaths: %@", [fm subpathsAtPath:PATH_STRING]);
-    }
-
-    [fm release];
 
     tex_file1 = fopen(PATH "Lucca_128_128.raw", "rb");
     if (tex_file1 && state->tex_buf1)
