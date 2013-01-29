d4e776ffee3b5be2e9ea22cfd4d5ea825f317b9b
kwigbo <jed@kwigbo.com>
2013-01-28 23:20:14 -0500

    Moved app specific code oue of the DisplayObject class.

--- VGDraw/Classes/KDisplay/KDisplayObject.m
+++ VGDraw/Classes/KDisplay/KDisplayObject.m
@@ -1,8 +1,5 @@
 #import "KDisplayObject.h"
 
-static VGfloat s_shift = 0.0f;
-static BOOL s_reverse = NO;
-
 @implementation KDisplayObject
 
 @synthesize width = width_;
@@ -15,41 +12,21 @@ static BOOL s_reverse = NO;
 	
 	if (self != nil)
 	{
-		self.backgroundColor = [KColor colorWithHex:0xffA3BEFF];
+		
 	}
 	
 	return self;
 }
 
+- (void)dealloc
+{
+	[backgroundColor_ release];
+	[super dealloc];
+}
+
 - (void)render
 {
-	VGfloat color[4] = { self.backgroundColor.red, 
-			self.backgroundColor.green, 
-			self.backgroundColor.blue, 
-			s_shift };
-			
-	vgSetfv(VG_CLEAR_COLOR, 4, color);
-	vgClear(0, 0, width_, height_);
-	vgFlush();
 	
-	if (!s_reverse)
-	{
-		s_shift += 0.01f;
-	}
-	else
-	{
-		s_shift -= 0.01f;
-	}
-	
-	if (s_shift >= 1.0)
-	{
-		s_reverse = YES;
-	}
-	else if (s_shift <= 0.0)
-	{
-		s_reverse = NO;
-		self.backgroundColor = [KColor randomColor];
-	}
 }
 
 @end
